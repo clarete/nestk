991955ab1e0dc8391f5cf04042778e3e2300ed4e
Lincoln de Sousa <lincoln@comum.org>
2020-01-15 22:09:05 -0500

    Add diff mode to nestest harness
    It is also handling Relative and AbsoluteX addressing mode formatting.
    Unfortunately, it's still not 100% accurate because I still don't get
    the meaning of a few addressing annotations.

--- nestest/nestest.js
+++ nestest/nestest.js
@@ -14,10 +14,14 @@ const hex = (data, padSize=2, padChr='0') => data
   .toUpperCase()
   .padStart(padSize, padChr);
 
-const formatParameter = (instruction, data) => {
+const formatParameter = (savedState, instruction, data) => {
   let suffix = '';
-  if (['STX'].includes(instruction.mnemonic))
-    suffix = ` = ${hex(cpu.x)}`;
+  switch (instruction.mnemonic) {
+  case 'STA': suffix = ` = ${hex(savedState.a)}`; break;
+  case 'STX': suffix = ` = ${hex(cpu.x)}`; break;
+  case 'STY': suffix = ` = ${hex(cpu.y)}`; break;
+  case 'BIT': suffix = ` = ${hex(cpu.a)}`; break;
+  }
   const formattedData = hex(data
     .slice(1)
     .reverse()
@@ -28,48 +32,83 @@ const formatParameter = (instruction, data) => {
     return suffix;
   case nes.AddrModes.Absolute:
   case nes.AddrModes.ZeroPage:
-  case nes.AddrModes.Relative:
     return `$${formattedData}${suffix}`;
   case nes.AddrModes.Immediate:
     return `#$${formattedData}${suffix}`;
+  case nes.AddrModes.AbsoluteX:
+    return `${formattedData}${suffix}`;
+  case nes.AddrModes.Relative:
+    return `$${hex(savedState.pc + instruction.size + parseInt(data[1], 16))}`;
   default:
     console.log(instruction);
     throw new Error(`Unknown addr mode ${instruction.addressingMode}`);
   }
 };
 
+const red = s => `\x1b[31m${s}\x1b[0m`;
+const green = s => `\x1b[33m${s}\x1b[0m`;
+
+const logLines = fs
+  .readFileSync('./nestest.log')
+  .toString()
+  .split('\n');
+
+function diff(i, a, b) {
+  // Ignore cycles & scanlines for now
+  const [aa] = a.split('CYC');
+  const [bb] = b.split('CYC');
+
+  if (aa !== bb) {
+    console.log(red(a));
+    console.log(green(b));
+  } else {
+    console.log(a);
+  }
+}
+
+function clone(obj) {
+  const c = Object.create(Object.getPrototypeOf(obj), Object.getOwnPropertyDescriptors(obj));
+  // c.bus = obj.bus.slice();
+  return c;
+}
+
 while (remaining-- > 0) {
-  const savedpc = cpu.pc;
-  const address = cpu.pc.toString(16).toUpperCase();
+  const savedState = clone(cpu);
+  const opcaddr = cpu.pc.toString(16).toUpperCase();
   const opcode = cpu.bus.read(cpu.pc);
   const instruction = nes.getinopc(opcode);
+  if (!instruction)
+    throw new Error(`UNKNOWN OPCODE: ${hex(opcode)}.`);
+  // Read the raw data of the instruction as well
   const data = [];
   for (let i = 0; i < instruction.size; i++)
     data.push(hex(cpu.bus.read(cpu.pc++)));
+  // Revert the Program Counter to the saved location right before the
+  // instructionwe just logged above
+  cpu.pc = savedState.pc;
+  // Run the one instruction on the CPU
+  cpu.step();
+
   const bigEndianData = data
-    .map(x => x.toString(16).toUpperCase())
+    .map(x => hex(x.toString(16)))
     .join(' ')
     .padEnd(8);
-  const lilEndianData = formatParameter(instruction, data).padEnd(27);
+
+  const address = formatParameter(savedState, instruction, data).padEnd(27);
   const logmsg = [
-    address, '',
+    opcaddr, '',
     bigEndianData, '',
     instruction.mnemonic,
-    lilEndianData,
-    `A:${hex(cpu.a)}`,
-    `X:${hex(cpu.x)}`,
-    `Y:${hex(cpu.y)}`,
-    `P:${hex(cpu.p)}`,
-    `SP:${hex(cpu.s)}`,
+    address,
+    `A:${hex(savedState.a)}`,
+    `X:${hex(savedState.x)}`,
+    `Y:${hex(savedState.y)}`,
+    `P:${hex(savedState.p)}`,
+    `SP:${hex(savedState.s)}`,
     `CYC:${hex(0, 3, ' ')}`,
     `SL:${0}`
   ].join(' ');
 
-  // Revert the Program Counter to the saved location right before the
-  // instructionwe just logged above
-  cpu.pc = savedpc;
-  // Run the one instruction on the CPU
-  cpu.step();
-  // Print out the stuff
-  console.log(logmsg);
+  // Compare and print out the stuff
+  diff(savedState, logmsg, logLines.shift());
 }
