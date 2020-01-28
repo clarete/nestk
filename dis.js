967b16dbe00677f25b4ba3416dfbcfd373a8c42c
Lincoln de Sousa <lincoln@comum.org>
2020-01-27 23:46:57 -0500

    dis: Hide illegal instructions from output of dis program

--- dis.js
+++ dis.js
@@ -4,23 +4,22 @@ const nes = require('./nes');
 const filename = './nestest/nestest.nes';
 const cpu = new nes.CPU6502(new nes.MemoryBus());
 const crt = nes.Cartridge.fromRomData(fs.readFileSync(filename));
-const pc = cpu.pc = 0x0C00;
+const pc = cpu.pc = 0xC000;
 
-for (const line of nes.dis6502code(crt.prg, pc, pc)) {
+for (const line of nes.dis6502code(crt.prg, pc)) {
   const { address, opcode, instruction, fmtop, rawdata } = line;
-  const mnemonic = instruction ? instruction.mnemonic : '???';
+  const mnemonic = (instruction && !instruction.illegal)
+    ? instruction.mnemonic
+    : '.db';
   const data = instruction
     ? rawdata.map(x => nes.safehex(x)).join(' ')
     : nes.hex(opcode);
-
   process.stdout.write(nes.hex(address, 4));
   process.stdout.write('   ');
   process.stdout.write(data.padEnd(8));
   process.stdout.write('   ');
   process.stdout.write(mnemonic);
-  if (fmtop) {
-    process.stdout.write(' ');
-    process.stdout.write(fmtop);
-  }
+  process.stdout.write(' ');
+  process.stdout.write(fmtop || `$${nes.hex(opcode)}`);
   process.stdout.write('\n');
 }
